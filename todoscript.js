fe60a41e1c0f4bc92fe14942aba6eab029669ba2
jessamelie <jessamelie.sola@gmail.com>
2023-03-17 10:25:32 +0100

    To do list version 1 OK

--- todoscript.js
+++ todoscript.js
@@ -2,51 +2,62 @@
 let taskInput = ""; //déclarer la variable
 let i = 0
 let text_task = ""
+let id_tasklist = document.getElementById("tasklist")
+let id_input = document.getElementById("input")
 
-function getInput(){
-taskInput = document.getElementById("input").value; //garde en mémoire le texte input
-document.getElementById("tasklist").innerHTML +=  
+//ajouter une tâche
+const getTask = () => {
+taskInput = id_input.value; //garde en mémoire le texte input
+
+id_tasklist.innerHTML +=  
 `<div class="removestep">
 <p class="newinput"> ${taskInput}</p> 
 <input type="checkbox" onClick="checkTask(event)" class="checkbox">
 <input type="button" class="remove" onClick="removeTask(event)" value="Supprimer">
 <input type="button" class="edit" onClick="editInput(event)" value="Editer"></div>`
-document.getElementById("input").value = "";   //suppression du texte input après ajout  
+
+id_input.value = "";   //suppression du texte input après ajout  
 return text_task = taskInput 
 }
 
-//éditer une tâche
-const editInput = (event) => {
-    const buttonedit = event.target
-    let askNewInput = prompt("Editer votre tâche ci-dessous");
-    const indexEdit = Array.from(document.querySelectorAll(".edit")).indexOf(buttonedit);
-    document.querySelectorAll(".newinput")[indexEdit].innerText = askNewInput
-    }
-
 //insertion tâche en appuyant sur Entrée
-document.getElementById("input").addEventListener("keypress",(event) => { 
+id_input.addEventListener("keypress",(event) => { 
     if (event.key === "Enter"){
-        getInput()
+        getTask()
     }
 })
 
+//éditer une tâche
+const editInput = (event) => {
+    let class_newinput = document.querySelectorAll(".newinput")
+    let class_edit = document.querySelectorAll(".edit")
+    const buttonedit = event.target
+    let askNewInput = prompt("Editer votre tâche ci-dessous");
+    const indexEdit = Array.from(class_edit).indexOf(buttonedit);
+    class_newinput[indexEdit].innerText = askNewInput
+    }
+
 //supprimer une tâche
 const removeTask = (event) => {
+let class_remove = document.querySelectorAll(".remove")
+let class_removeStep = document.querySelectorAll(".removestep")
 const buttonremove = event.target //l'event du click cible
-const index = Array.from(document.querySelectorAll(".remove")).indexOf(buttonremove); //On cherche dans le tableau de (Array.from) de la class "remove" l'index de l'event.target
-document.querySelectorAll(".removestep")[index].remove()
+const index = Array.from(class_remove).indexOf(buttonremove); //On cherche dans le tableau de (Array.from) de la class "remove" l'index de l'event.target
+class_removeStep[index].remove()
 }
 
 //rayez une tâche
 const checkTask = (event) => {
+    let class_newinput = document.querySelectorAll(".newinput")
+    let class_checkbox = document.querySelectorAll(".checkbox")
     const task_target = event.target
-    const index = Array.from(document.querySelectorAll(".checkbox")).indexOf(task_target)
-    if (document.querySelectorAll(".checkbox")[index].checked == true)
+    const index = Array.from(class_checkbox).indexOf(task_target)
+    if (class_checkbox[index].checked == true)
     {
-        document.querySelectorAll(".newinput")[index].innerHTML = `<strike>${document.querySelectorAll(".newinput")[index].innerText}</strike>`
+        class_newinput[index].innerHTML = `<strike>${class_newinput[index].innerText}</strike>`
     }
-    else if (document.querySelectorAll(".checkbox")[index].checked == false)
+    else if (class_checkbox[index].checked == false)
     {
-        document.querySelectorAll(".newinput")[index].innerText = document.querySelectorAll(".newinput")[index].innerText
+        class_newinput[index].innerText = class_newinput[index].innerText
     }
 }
